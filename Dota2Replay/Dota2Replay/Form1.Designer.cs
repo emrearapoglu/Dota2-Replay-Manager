a3b1b84acecfd99695ab561074bc0609b0eb5ae1
fplayer <arapogluemre@gmail.com>
2015-02-26 01:28:09 +0200

    First release

---
+++ Dota2Replay/Dota2Replay/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Dota2Replay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.watchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addDescriptionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.FullRowSelect = true;
+            this.listView1.LabelEdit = true;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.listView1.Size = new System.Drawing.Size(1021, 521);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
+            this.listView1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseMove);
+            this.listView1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseUp);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Match ID";
+            this.columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "File Size";
+            this.columnHeader2.Width = 89;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Creation Date";
+            this.columnHeader3.Width = 155;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Dotabuff Link(Click to open)";
+            this.columnHeader4.Width = 161;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Description";
+            this.columnHeader5.Width = 370;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteToolStripMenuItem,
+            this.watchToolStripMenuItem,
+            this.addDescriptionToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(160, 70);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // watchToolStripMenuItem
+            // 
+            this.watchToolStripMenuItem.Name = "watchToolStripMenuItem";
+            this.watchToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.watchToolStripMenuItem.Text = "Watch";
+            this.watchToolStripMenuItem.Click += new System.EventHandler(this.watchToolStripMenuItem_Click);
+            // 
+            // addDescriptionToolStripMenuItem
+            // 
+            this.addDescriptionToolStripMenuItem.Name = "addDescriptionToolStripMenuItem";
+            this.addDescriptionToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.addDescriptionToolStripMenuItem.Text = "Add Description";
+            this.addDescriptionToolStripMenuItem.Click += new System.EventHandler(this.addDescriptionToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 541);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(439, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 544);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Replay Directory";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(545, 539);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Change Directory";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(922, 544);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 566);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Dota2 Replay Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem watchToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem addDescriptionToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
+
---
+++ Dota2Replay/Dota2Replay/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Dota2Replay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static class Globals {
+            public static string replaySource = @System.IO.File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\dota2 replay editor\\replayfolder.txt");
+            public static string descSource = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\dota2 replay editor\\description.txt";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!System.IO.Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\dota2 replay editor"))
+            {
+                System.IO.Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\dota2 replay editor");
+                //File.Create(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\dota2 replay editor\\replayfolder.txt");
+                TextWriter tw = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\dota2 replay editor\\replayfolder.txt");
+                FolderBrowserDialog replayFolderDialog = new FolderBrowserDialog();
+                DialogResult result = replayFolderDialog.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    tw.Write(replayFolderDialog.SelectedPath);
+                    tw.Close();
+                    Application.Restart();
+                }
+            }
+            else
+            {
+                textBox1.Text = Globals.replaySource;
+                loadReplays();
+                getDescriptionsFromFile();
+            }
+        }
+
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (listView1.FocusedItem.Bounds.Contains(e.Location) == true)
+                {
+                    contextMenuStrip1.Show(Cursor.Position);
+                }
+            } 
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String text = listView1.SelectedItems[0].Text;
+            string replay = Globals.replaySource + "\\" + text + ".dem";
+            DialogResult replaydelete = MessageBox.Show("Are you sure you want to delete this replay?\n\n" + replay, "Delete Replay", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+            if(replaydelete == DialogResult.Yes)
+            {
+                File.Delete(replay);
+                MessageBox.Show("Replay #"+text+" is successfully deleted.");
+                loadReplays();
+                getDescriptionsFromFile();
+            }
+            if(replaydelete == DialogResult.No)
+            {
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            selectFolder();
+        }
+
+        private void selectFolder()
+        {
+            FolderBrowserDialog replayFolderDialog = new FolderBrowserDialog();
+            DialogResult result = replayFolderDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = replayFolderDialog.SelectedPath;
+                Globals.replaySource = replayFolderDialog.SelectedPath;
+                loadReplays();
+                getDescriptionsFromFile();
+            }
+        }
+
+        private void getDescriptionsFromFile()
+        {
+            string path = Globals.descSource;
+            if (!File.Exists(path))
+            {
+                //File.Create(path);
+                TextWriter tw = new StreamWriter(path);
+                tw.Close();
+            }
+            else if (File.Exists(path))
+            {
+                closeFile();
+                foreach (ListViewItem itemRow in this.listView1.Items)
+                {
+                    for (int i = 0; i < listView1.Items.Count; i++)
+                    {
+                        var searchTarget = listView1.Items[i].SubItems[0].Text;
+                        foreach (var line in File.ReadLines(Globals.descSource))
+                        {                        
+                            if (line.Contains(searchTarget))
+                            {
+                                string[] values = line.Split('=');
+                                string value = values[1];
+                                listView1.Items[i].SubItems[4].Text = value;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void loadReplays()
+        {
+            listView1.Items.Clear();
+            if(System.IO.Directory.Exists(Globals.replaySource))
+            {
+                string[] direction = Directory.GetFiles(Globals.replaySource, "*.dem");
+                int count = 0;
+                foreach (string file in direction)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(file);
+                    FileInfo fi = new FileInfo(file);
+                    var size = fi.Length;
+                    double MB = (Convert.ToDouble(size) / 1024) / 1024;
+                    double roundSize = Math.Round(MB, 2);
+                    string[] row1 = { roundSize.ToString() + " MB", fi.LastWriteTime.ToString(), fileName, "No Description" };
+                    listView1.Items.Add(fileName).SubItems.AddRange(row1);
+                    count++;
+                }
+                label2.Text = count + " replays found";
+            }
+        }
+
+        private void listView1_MouseMove(object sender, MouseEventArgs e)
+        {
+            var hit = listView1.HitTest(e.Location);
+            if (hit.SubItem != null && hit.SubItem == hit.Item.SubItems[3]) listView1.Cursor = Cursors.Hand;
+            else listView1.Cursor = Cursors.Default;
+        }
+    
+        private void listView1_MouseUp(object sender, MouseEventArgs e)
+        {
+            var hit = listView1.HitTest(e.Location);
+            if (hit.SubItem != null && hit.SubItem == hit.Item.SubItems[3])
+            {
+                var url = "http://www.dotabuff.com/matches/"+hit.SubItem.Text;
+                System.Diagnostics.Process.Start(url);
+            }
+        }
+
+        private void closeFile()
+        {
+            string path = Globals.descSource;
+            FileStream fs = new FileStream(path, FileMode.OpenOrCreate);
+            fs.Close();
+
+        }
+
+        private void addDescriptionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String text = listView1.SelectedItems[0].Text;
+            string input = Microsoft.VisualBasic.Interaction.InputBox("Type a description for replay"+text, "Set Description", "2k mmr", -1, -1);
+            using (FileStream fs = new FileStream(Globals.descSource, FileMode.Append, FileAccess.Write))
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                sw.WriteLine(text + " = " + input);
+            }
+            getDescriptionsFromFile();
+        }
+
+        private void watchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String text = listView1.SelectedItems[0].Text;
+            MessageBox.Show("Console command for watching replay #"+text+" has been copied to your clipboard.\nOpen DOTA 2 and paste it to console.");
+            System.Windows.Forms.Clipboard.SetText("playdemo replays/"+text+".dem");
+        }
+    }
+
+}
